97d7b43e08922056f721db226a7da14fa6c463e8
Taha daboussi <Taha_daboussi@hotmail.com>
2024-08-31 17:35:54 +0300

    added buy action button and improvments

--- DiscordBot/Main.ts
+++ DiscordBot/Main.ts
@@ -1,30 +1,21 @@
 // src/index.ts
-import { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } from 'discord.js';
-import * as dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Interaction, ButtonInteraction } from 'discord.js';
 import User from '../Database/Database';
 import { getCoinData } from '../Helpers/CoinInfo/GetCoinCMC'
 import { Utils } from '../Helpers/Utils';
-dotenv.config();
-
+import { buyCoinFromBinance, buyCoinFromBybit } from './BuyActions'
+import { connectDB } from '../Database/Database'
+let userCache: Record<string, any> = {};
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-async function connectDB() {
-    try {
-        await mongoose.connect("mongodb+srv://blackent151:Ly2HF7qJIOeDkyZD@cluster0.qzd08.mongodb.net/" as string)
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-    }
-}
-connectDB();
+
 
 const PREFIX = '!';
-
+const BOT_TOKEN = "MTI3ODY3NTgxMjYxMDIxMTg1MQ.GDfuXe.AqwYfPcqSThylodhPzwxE-vABvjbhaKFxd8b48"
 const handleSetBinanceApiKey = async (message: any) => {
-    if (message && message.content &&  message.content.startsWith(`${PREFIX}addbinance`)) {
+    if (message && message.content && message.content.startsWith(`${PREFIX}addbinance`)) {
         const args = message.content.split(' ').slice(1);
         if (args.length !== 2) {
             return message.reply('Please provide both an API Key and Secret. Usage: `!addbinance <apiKey> <apiSecret>`');
@@ -46,7 +37,7 @@ const handleSetBinanceApiKey = async (message: any) => {
     }
 }
 const handleSetByBitApiKey = async (message: any) => {
-    if (message && message.content &&  message.content.startsWith(`${PREFIX}addbybit`)) {
+    if (message && message.content && message.content.startsWith(`${PREFIX}addbybit`)) {
         const args = message.content.split(' ').slice(1);
         if (args.length !== 2) {
             return message.reply(
@@ -70,7 +61,7 @@ const handleSetByBitApiKey = async (message: any) => {
     }
 }
 const handleSetAmount = async (message: any) => {
-    if (message && message.content &&  message.content.startsWith('!setamount')) {
+    if (message && message.content && message.content.startsWith('!setamount')) {
         const args = message.content.split(' ').slice(1);
         if (args.length !== 1) {
             return message.reply('Please provide an amount. Usage: `!setamount <amount>`');
@@ -95,7 +86,37 @@ const handleSetAmount = async (message: any) => {
         }
     }
 }
+const replayToButtonInteraction = async (buttonInteraction: ButtonInteraction, content: string) => {
+    try {
+        await buttonInteraction.reply({ content: content, ephemeral: true })
+    } catch (err) {
+        Utils.log('Error in replaying to button interaction ' + err, 'error')
+    }
+}
+const dataErrorHandling = (amount : number  , apiKey : string , apiSecret : string ,buttonInteraction  : ButtonInteraction) => {
+    if(!amount || !apiKey || !apiSecret){
+        replayToButtonInteraction(buttonInteraction, "Please set your amount and API keys first")
+        return false 
+    }
+    return true 
+}
+async function fetchAndCacheDatabase() {
+    try {
+        // Connect to MongoDB
 
+        // Fetch all user documents
+        const users = await User.find({});
+
+        // Update the cache
+        userCache = users.reduce((cache, user) => {
+            cache[user.discordTag] = user;
+            return cache;
+        }, {} as Record<string, any>);
+
+    } catch (error) {
+        console.error('Error fetching data from MongoDB:', error);
+    }
+}
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user?.tag}`);
@@ -114,29 +135,28 @@ client.on('messageCreate', async (message) => {
         await message.channel.send({ embeds: [helpEmbed] });
     }
 
-    if (message  && message.embeds && message.embeds[0] && message.embeds[0].data.title === 'New Listing Catched' && message.author.id == "1277970962297393172") {
+    if (message && message.embeds && message.embeds[0] && message.embeds[0].data.title === 'New Listing Catched' && message.author.id == "1277970962297393172") {
         const description = message.embeds[0].data.description as string
         const embed = new EmbedBuilder()
             .setTitle('New Listing Catched')
             .setDescription(message.embeds[0].data.description || "No Description")
-            .addFields(
-                ...(message.embeds[0].data.fields || [])
-            ).setColor(0xFFCAF1);;
+            .addFields(...(message.embeds[0].data.fields || []))
+            .setColor(0xFFCAF1);;
+
         const CoinName = Utils.extractCoinNames(description);
         CoinName.forEach(async (coinName) => {
-            getCoinData(coinName, "https://discord.com/api/webhooks/1277970962297393172/37htilEDkqC2RSHwWuDqZPAo5GfLScyaHfZigfNsMwFEb9ih-YRRD46-qE4_GRZaPK3X")
-            
+            const webhook = "https://discord.com/api/webhooks/1277970962297393172/37htilEDkqC2RSHwWuDqZPAo5GfLScyaHfZigfNsMwFEb9ih-YRRD46-qE4_GRZaPK3X"
+            getCoinData(coinName, webhook)
+
+            const buyBinance = new ButtonBuilder().setCustomId(`💰 Buy on Binance | Coin: ${coinName}`).setLabel(`💰 Buy on Binance | Coin: ${coinName}`).setStyle(ButtonStyle.Primary);
+            const buyBybit = new ButtonBuilder().setCustomId(`💰 Buy on bybit | Coin: ${coinName}`).setLabel(`💰 Buy on bybit | Coin: ${coinName}`).setStyle(ButtonStyle.Primary);
+            const row = new ActionRowBuilder<ButtonBuilder>().addComponents(buyBinance, buyBybit);
+
+            await message.channel.send({ embeds: [embed], components: [row] });
         })
-        const buyBinance = new ButtonBuilder().setCustomId('BUY(BINANCE)').setLabel('BUY(BINANCE)').setStyle(ButtonStyle.Primary);
-        const buyBybit= new ButtonBuilder().setCustomId('BUY(BYBIT)').setLabel('BUY(BYBIT)').setStyle(ButtonStyle.Primary);
-        const longBinance = new ButtonBuilder().setCustomId('LONG(BINANCE)').setLabel('LONG(BINANCE)').setStyle(ButtonStyle.Primary);
-        const longBybit = new ButtonBuilder().setCustomId('LONG(BYBIT)').setLabel('LONG(BYBIT)').setStyle(ButtonStyle.Primary);
-        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(buyBinance,buyBybit,longBinance,longBybit);
-
-        await message.channel.send({ embeds: [embed], components: [row] });
     }
-    // Command to add Binance API key
 
+    // Command to add Binance API key
     handleSetBinanceApiKey(message)
     // Command to add Bybit API key
     handleSetByBitApiKey(message)
@@ -144,5 +164,39 @@ client.on('messageCreate', async (message) => {
     handleSetAmount(message)
 });
 
+// TODO to refactor 
+client.on('interactionCreate', async (interaction: Interaction) => {
+    if (!interaction.isButton()) return; // Ensure the interaction is a button click
+    const buttonInteraction = interaction as ButtonInteraction;
 
-client.login("MTI3ODY3NTgxMjYxMDIxMTg1MQ.GDfuXe.AqwYfPcqSThylodhPzwxE-vABvjbhaKFxd8b48");
+    if (!userCache[interaction.user.username]) replayToButtonInteraction(buttonInteraction, 'Please set your amount and API keys first');
+    const customId = buttonInteraction.customId; // Retrieve the custom ID of the clicked button
+    const {amount , binanceApiKey , binanceApiSecret , bybitApiKey , bybitApiSecret} = userCache[interaction.user.username]._doc
+    // Handle button interaction based on the custom ID
+    if (customId && customId.includes('💰 Buy on Binance | Coin:')) {
+        const coin = customId.split('💰 Buy on Binance | Coin: ')[1];
+
+        const canBuyCoinFromBuybit = dataErrorHandling(amount,binanceApiKey,binanceApiSecret, buttonInteraction)
+        if(!canBuyCoinFromBuybit) return
+
+        const response = await buyCoinFromBinance(coin + "USDT", amount, binanceApiKey, binanceApiSecret);
+        replayToButtonInteraction(buttonInteraction, response.toString())
+    } else if (customId && customId.includes('💰 Buy on bybit | Coin:')) {
+        const coin = customId.split('💰 Buy on bybit | Coin: ')[1];
+        const canBuyCoinFromBuybit = dataErrorHandling(amount,bybitApiKey,bybitApiSecret, buttonInteraction)
+        if(!canBuyCoinFromBuybit) return
+
+        const response = await buyCoinFromBybit(coin + "USDT", amount.toString(), bybitApiKey, bybitApiSecret);
+        replayToButtonInteraction(buttonInteraction, response.toString())
+    }
+});
+
+
+client.on('error', (error) => {
+    Utils.log('An error occurred: in discord Monitor ' + error, 'error');
+})
+
+client.login(BOT_TOKEN);
+setInterval(fetchAndCacheDatabase, 5 * 1000);
+fetchAndCacheDatabase();
+connectDB();
