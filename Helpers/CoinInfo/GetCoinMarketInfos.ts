fb52753b1ee185e14a4d24c70565ff92a9e95502
Taha daboussi <Taha_daboussi@hotmail.com>
2024-08-31 17:36:11 +0300

    only return top 5 markets rather than all and only the USDT pairs

--- Helpers/CoinInfo/GetCoinMarketInfos.ts
+++ Helpers/CoinInfo/GetCoinMarketInfos.ts
@@ -8,8 +8,8 @@ export const getCoinTopExchanges = async (tokenName: string): Promise<any> => {
     try{
         const response :any = await axios.get(url)
         if(response && response.data && response.data.data.marketPairs){
-            const names = response.data.data.marketPairs.map((pair:any) => {return {name : pair.exchangeName , url: pair.marketUrl} })
-            return names.slice(0,5)
+            const names = response.data.data.marketPairs.map((pair:any) => {return {name : pair.exchangeName , url: pair.marketUrl} }).filter((data :{name : string  , url : string } )=>data.url.includes("USD"))
+            return names.length > 5  ? names.slice(0,5) : names
         }
         throw new Error('Error in fetching data from CoinMarketCap API')
     }catch(err){
