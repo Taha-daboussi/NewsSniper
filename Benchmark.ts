19a2fad36bab7af9e92d9f9df091a84ec70f6b6f
Taha daboussi <Taha_daboussi@hotmail.com>
2024-09-03 14:32:49 +0300

    small changes

--- Benchmark.ts
+++ Benchmark.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Main } from './Modules/UpBit/Main';
 // Function to measure response time
 async function benchmarkAPI(url : string , requests = 10) {
     const responseTimes = [];
@@ -7,19 +8,11 @@ async function benchmarkAPI(url : string , requests = 10) {
         const startTime = Date.now(); // Start time
 
         try {
-            const response : any  = await axios.get(url , {
-                headers : {
-                     
-                }
-            }); // Make the request
+            const response : any  = await  new Main().FrontendRequests.getNews() // Make the request
             const endTime = Date.now(); // End time
 
             const duration = endTime - startTime; // Calculate response time
             responseTimes.push(duration);
-            const result =  response.data.availableSymbols.reduce((latest : any , item: any ) => {
-                return new Date(item.availableSince) > new Date(latest.availableSince) ? item : latest;
-              }, response.data);
-            return result 
             console.log(`Request ${i + 1}: ${duration} ms - Status: ${response.status}`);
         } catch (err : any ) {
             console.error(`Request ${i + 1} failed: ${err.message}`);
@@ -36,10 +29,12 @@ async function benchmarkAPI(url : string , requests = 10) {
     console.log(`Average Response Time: ${averageTime.toFixed(2)} ms`);
     console.log(`Min Response Time: ${Math.min(...responseTimes)} ms`);
     console.log(`Max Response Time: ${Math.max(...responseTimes)} ms`);
+    process.exit()
+    return ;
 }
 
 // Upbit API URL
 const url = 'https://api.tardis.dev/v1/exchanges/upbit';
 
 // Run benchmark with 10 requests
-benchmarkAPI(url, 1000);
+benchmarkAPI(url, 100);
